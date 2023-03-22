6f1b8c8c4de600ee0ca7830fbaaf2887b289ff5a
agorbanev <gorbanev.am@ya.ru>
2023-03-22 13:52:44 +0300

    0.0.3

--- build_tf/yabuild.tf
+++ build_tf/yabuild.tf
@@ -29,7 +29,7 @@ resource "yandex_compute_instance" "vm-1" {
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu_image.id
-    #   type = "network-ssd"
+      type = "network-ssd"
       size = 20
     }
   }
