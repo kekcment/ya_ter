79ba6e624348132cecb0efaf252c46808701b686
agorbanev <gorbanev.am@ya.ru>
2023-03-22 12:51:43 +0300

    0.0.2

--- build_tf/yabuild.tf
+++ build_tf/yabuild.tf
@@ -73,16 +73,16 @@ resource "yandex_compute_instance" "vm-2" {
 
 }
 
-resource "yandex_vpc_network" "network_terraform" {
-  name = "net_terraform"
-}
+# resource "yandex_vpc_network" "network_terraform" {
+#   name = "net_terraform"
+# }
 
-resource "yandex_vpc_subnet" "subnet_terraform" {
-  name           = "sub_terraform"
-  zone           = "ru-central1-b"
-  network_id     = yandex_vpc_network.network_terraform.id
-  v4_cidr_blocks = ["192.168.15.0/24"]
-}
+# resource "yandex_vpc_subnet" "subnet_terraform" {
+#   name           = "sub_terraform"
+#   zone           = "ru-central1-b"
+#   network_id     = yandex_vpc_network.network_terraform.id
+#   v4_cidr_blocks = ["192.168.15.0/24"]
+# }
 
 # scheduling_policy {
 #     preemptible = true
