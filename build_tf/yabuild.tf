f21eec45d548ad8263d2164c6ed8f29393f9a840
agorbanev <gorbanev.am@ya.ru>
2023-03-22 13:51:56 +0300

    0.0.3

--- build_tf/yabuild.tf
+++ build_tf/yabuild.tf
@@ -50,50 +50,50 @@ resource "yandex_compute_instance" "vm-1" {
 }
 
 
-resource "yandex_compute_instance" "vm-2" {
-  name = "vm-2"
+# resource "yandex_compute_instance" "vm-2" {
+#   name = "vm-2"
 
-  resources {
-    cores  = 2
-    memory = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.ubuntu_image.id
-    #   type = "network-ssd"
-      size = 20
-    }
-  }
-
-  network_interface {
-    subnet_id = "e2lvp1dpmlcm62r0r221"
-    nat       = true
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file("/var/lib/jenkins/.ssh/build_key.pub")}"
-  }
-
-    scheduling_policy {
-        preemptible = true
-  }
-}
-
-
-# connection {
-#     type = "ssh"
-#     user = "ubuntu"
-#     private_key = file("/var/lib/jenkins/.ssh/build_key")
-#     host = self.network_interface[0].nat_ip_address
+#   resources {
+#     cores  = 2
+#     memory = 2
 #   }
 
-#   provisioner "remote-exec" {
-#     inline = [
-#       "sudo apt update && sudo apt install python -y"
-#     ]
+#   boot_disk {
+#     initialize_params {
+#       image_id = data.yandex_compute_image.ubuntu_image.id
+#     #   type = "network-ssd"
+#       size = 20
+#     }
 #   }
 
-# provisioner "local-exec" {
-#     command = "echo > /tmp/test1 && echo '[build]' > /tmp/test1 && echo ${self.network_interface[0].nat_ip_address} >> /tmp/test1"
+#   network_interface {
+#     subnet_id = "e2lvp1dpmlcm62r0r221"
+#     nat       = true
 #   }
+
+#   metadata = {
+#     ssh-keys = "ubuntu:${file("/var/lib/jenkins/.ssh/build_key.pub")}"
+#   }
+
+#     scheduling_policy {
+#         preemptible = true
+#   }
+# }
+
+
+connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("/var/lib/jenkins/.ssh/build_key")
+    host = self.network_interface[0].nat_ip_address
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update && sudo apt install python -y"
+    ]
+  }
+
+provisioner "local-exec" {
+    command = "echo > /tmp/test1 && echo '[build]' > /tmp/test1 && echo ${self.network_interface[0].nat_ip_address} >> /tmp/test1"
+  }
